86ff8ad77d5a71df1b102f5761a9ae4d55acb1f9
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-11-05 11:20:18 +0300

    Copy to clipboard functionality added

--- images/views.py
+++ images/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
 from .models import Image
+from decouple import config,Csv
+
 
 # Create your views here.
 def images(request):
 
     images=Image.objects.all()
+    ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
-    return render(request,'images.html',{"images":images})
+    return render(request,'images.html',{"images":images,"ALLOWED_HOSTS":ALLOWED_HOSTS})
 
 def search_results(request):
     if 'image' in request.GET and request.GET["image"]:
