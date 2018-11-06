59c9ad138f433aa36bc23e02d5dba7c6db448d7b
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-11-06 09:41:18 +0300

    Deployment

--- images/tests.py
+++ images/tests.py
@@ -45,10 +45,20 @@ class categoriesTestClass(TestCase):
 
 class ImageTestClass(TestCase):
     def setUp(self):
-        self.category = categories(category='Food')
-        self.category.save_category()
+        self.test_category = categories(category=list('Travel'))
+        self.test_category.save_category()
 
         self.location = Location(location="Moringa")
         self.location.save_location()
 
-        self.image = Image(title="Pl")
+        self.image = Image(id=1,title="Slide Away",categories=self.test_category,location=self.location,)
+        self.image.save_image()
+
+    def tearDown(self):
+        categories.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
+
+    # def test_save_image(self):
+    #     images = Image.objects.all()
+    #     self.assertTrue(len(images)>0)
